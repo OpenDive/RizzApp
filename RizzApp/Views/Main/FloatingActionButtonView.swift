12ffd031b5369459d5c0c86a929f3f10827b90fa
MarcoDotIO <Marcus.Arnett10@gmail.com>
2023-09-23 15:22:29 -0400

    Implement Event Details View

--- RizzApp/Views/Main/FloatingActionButtonView.swift
+++ RizzApp/Views/Main/FloatingActionButtonView.swift
@@ -13,6 +13,26 @@ struct FloatingActionButtonView: View {
             Spacer()
             
             HStack {
+                HStack {
+                    Image(systemName: "arkit")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .padding(.leading, 4)
+                    
+                    Image(systemName: "arkit")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .padding(.leading, 30)
+                }
+                .background {
+                    Capsule()
+                        .foregroundStyle(RizzColors.rizzBlue)
+                        .frame(width: 190, height: 110)
+                }
+                .padding(.leading, 40)
+                
                 Spacer()
                 
                 Circle()
