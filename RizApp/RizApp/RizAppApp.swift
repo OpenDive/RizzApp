caf5f44893572d049f4aea6f8639279b209ffd66
MarcoDotIO <Marcus.Arnett10@gmail.com>
2023-09-23 03:24:43 -0400

    Implement initial onboarding UI

---
+++ RizApp/RizApp/RizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RizAppApp.swift
+//  RizApp
+//
+//  Created by Marcus Arnett on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct RizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RizApp/RizApp/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  RizApp
+//
+//  Created by Marcus Arnett on 9/22/23.
+//
+
+import Foundation
